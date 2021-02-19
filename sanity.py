41ea556cc488de94183192ad2a6c3764a10c8e60
Mika Vainio <koti.mika.vainio@gmail.com>
2021-02-19 15:28:47 +0200

    Lisätty TODOt hiihtoloman jälkeiseen elämmään

--- sanity.py
+++ sanity.py
@@ -25,26 +25,31 @@ def liukuluku_syote(syote):
     if syote.find('.') != -1:  # Katsotaan löytyykö pistettä
         osat = syote.split('.') # luodaan lista jossa pisteellä erotetut osat syötteestä
         if len(osat) > 2: # Jos osia on enemmän kuin 2, pisteitä on liikaa
-            arvo = 0
-            virhekoodi = 1
+            arvo = 0 # Virheellisen tiedon tapauksessa annetaan arvoksi 0
+            virhekoodi = 1 # Tämä on virhe nro 1
             virhesanoma = 'Syötteessä useita desimaalierottimia'
         else: # Osia korkeintaan 2
             osa = str(osat[0]) # Otetaan ensimmäinen listan ensimmäinen arvo ja muutetaan merkkijonoksi
             if osa.isnumeric(): # Jos se on numeerinen
                 osa = str(osat[1]) # Otetaan toinen osa
-
+            
                 if osa.isnumeric(): # Jos toinenkin osa on numeeerinen
                     arvo = float(syote) # Muutetaan syöte liukuluvuksi
                     virhekoodi = 0 # Paluukoodi 0 -> ei virhettä
                     virhesanoma = 'Syöte OK'
-          
-    else:
-        if syote.isnumeric():
-            arvo = float(syote)
-            virhekoodi = 0
+                else: # TODO: Tähän virhekoodin 2 asettaminen
+                    pass
+                
+            else:  # TODO: Tähän virhekoodin 2 asettaminen
+                pass
+
+    else: # Jos pistettä ei löytynyt
+        if syote.isnumeric(): # Onko syöte numeerinen
+            arvo = float(syote) # Muutetaan liukuluvuksi
+            virhekoodi = 0 # 0 -> ei virhettä
             virhesanoma = 'Syöte OK'
           
-      
+    # TODO: Koosta arvo ja virhetiedot listaksi tulokset  
     return tulokset
 
 
