77cd09417273f1c99c1eee1c1e9494b789b852af
Mika Vainio <koti.mika.vainio@gmail.com>
2021-02-19 14:56:24 +0200

    Korjattu typo syöte -> syote

--- sanity.py
+++ sanity.py
@@ -19,21 +19,23 @@ def liukuluku_syote(syote):
         list: virhekoodi (int), sanoma (string), arvo (float)
     """
     syote = syote.strip() # Poistetaan tulostumattomat merkit molemmista päistä
-    if syöte.find(',') != -1: # Katsotaan sisältääkö pilkkuja
+    if syote.find(',') != -1: # Katsotaan sisältääkö pilkkuja
         syote = syote.replace(',', '.') # Korvataan pilkut pisteillä
+
     if syote.find('.') != -1:  # Katsotaan löytyykö pistettä
         osat = syote.split('.') # luodaan lista jossa pisteellä erotetut osat syötteestä
-        if len(osat) > 2:
+        if len(osat) > 2: # Jos osia on enemmän kuin 2, pisteitä on liikaa
             arvo = 0
             virhekoodi = 1
             virhesanoma = 'Syötteessä useita desimaalierottimia'
-        else:
-            osa = str(osat[0])
-            if osa.isnumeric():
-               osa = str(osat[1]) 
-                if osa.isnumeric():
-                    arvo = float(syote)
-                    virhekoodi = 0
+        else: # Osia korkeintaan 2
+            osa = str(osat[0]) # Otetaan ensimmäinen listan ensimmäinen arvo ja muutetaan merkkijonoksi
+            if osa.isnumeric(): # Jos se on numeerinen
+                osa = str(osat[1]) # Otetaan toinen osa
+
+                if osa.isnumeric(): # Jos toinenkin osa on numeeerinen
+                    arvo = float(syote) # Muutetaan syöte liukuluvuksi
+                    virhekoodi = 0 # Paluukoodi 0 -> ei virhettä
                     virhesanoma = 'Syöte OK'
           
     else:
